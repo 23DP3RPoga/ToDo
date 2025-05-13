958fb1e7aef1b97ba3c3cdea8280a9eb7b6cd92b
23DP3RPPoga <A231753rp@rvt.lv>
2025-05-13 22:49:25 +0300

    Pabeigts frontend

--- ToDo/backend/server.js
+++ ToDo/backend/server.js
@@ -1,26 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
+const path = require('path');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
-
-mongoose.connect('mongodb://localhost:27017/todolist', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-const taskRoutes = require('./routes/tasks');
-app.use('/tasks', taskRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.use(express.static(path.join(__dirname, '..', 'frontend')));  
 
 app.get('/', (req, res) => {
-    res.send('Serveris darbojas!');
-  });
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html')); 
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
